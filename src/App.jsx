11f7cdeef36a2d09648bc79a5223b6e1865eb868
mansourlahham14 <mansourlahham14@gmail.com>
2026-01-12 17:02:20 +0100

    sign in/up/out and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Home from "./pages/Home/Home"
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from "./pages/Login/Login"
 import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, async(user)=>{
+      if(user){
+        console.log("Logged In");
+        navigate("/")
+      }else{
+        console.log("Logged Out");
+        navigate("/login")
+      }
+    })
+  },[])
+
   return (
     <>
+     <ToastContainer theme='dark'/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
